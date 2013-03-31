aa776e3dbb1b1fc396f6803a4cb9b0b474d1cb57
Santiago Carrillo <sancarbar@gmail.com>
2013-03-31 11:48:50 +0200

    Class license comment added.

--- src/com/sancarbar/androidcalendar/util/CalendarUtil.java
+++ src/com/sancarbar/androidcalendar/util/CalendarUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Santiago Carrillo Barbosa - Android Calendar Picker
+ * email: sancarbar@gmail.com
+ *
+ * https://github.com/sancarbar/Android-Calendar-Picker
+ *
+ * (C) 2013, Santiago Carrillo
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 3 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
 package com.sancarbar.androidcalendar.util;
 
 import android.content.Context;
